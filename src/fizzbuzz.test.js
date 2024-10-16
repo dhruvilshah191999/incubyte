3bafe8e941af0a3688a9836a44f27e3d96ebbe15
Dhruvil Shah <59329841+dhruvilshah191999@users.noreply.github.com>
2024-10-16 22:27:55 +0530

    Test: Add test for divisible by 5 (Buzz)

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -3,3 +3,7 @@ const fizzBuzz = require("./fizzbuzz");
 test('should return "Fizz" for numbers divisible by 3', () => {
   expect(fizzBuzz(3)).toBe("Fizz");
 });
+
+test('should return "Buzz" for numbers divisible by 5', () => {
+  expect(fizzBuzz(5)).toBe("Buzz");
+});
